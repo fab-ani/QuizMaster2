3da49cc36ac5ba7d9fe53333356d9cb97ee1bfb7
fab-ani <fabianhalis76@gmail.com>
2024-03-12 07:41:55 -0700

    use try to usecontent

--- src/contentTypeOptions.cjs
+++ src/contentTypeOptions.cjs
@@ -1,6 +1,6 @@
 export default function(req, res, next){
-    res.setHeader('X-Content-Type-Options', 'nosniff')
+    res.setHeader('X-Content-Type-Options', 'nosniff');
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    res.setHeader('Cache-Control', 'max-age=31536000, immutable');
+    res.setHeader('Cache-Control',  'public, max-age=31536000, immutable');
     next();
 };
