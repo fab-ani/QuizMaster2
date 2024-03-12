83092b3f3b4d42136b9871b6e508c5439029854d
fab-ani <fabianhalis76@gmail.com>
2024-03-12 08:16:10 -0700

    use try to usecontentTyp\\e

--- src/contentTypeOptions.cjs
+++ src/contentTypeOptions.cjs
@@ -1,6 +1,14 @@
 export default function(req, res, next){
     res.setHeader('X-Content-Type-Options', 'nosniff');
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    
+    if(req.url !== '/' && !req.url.endsWith('.html')){
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+
+    }else{
+        res.setHeader('Cache-Control', 'no-store');
+
+    }
+
     res.setHeader('Cache-Control',  'public, max-age=31536000, immutable');
     next();
 };
