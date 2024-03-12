4136d5f0e6489dc83fed8adebec4997f8aa89dd2
fab-ani <fabianhalis76@gmail.com>
2024-03-12 02:36:10 -0700

    use try to usecontentType

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig,loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import contentTypeOptions from './src/contentTypeOptions.cjs';
 
 export default ({ mode }) =>{
   const env = loadEnv(mode, process.cwd());
@@ -18,4 +19,7 @@ export default ({ mode }) =>{
   build: {
     chunkSizeWarningLimit: 1600,
   },
+  server:{
+    middleware:[contentTypeOptions],
+  }
 })}
